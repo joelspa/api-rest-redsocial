c42d18cf305bae407af198336e4757b4948a024c
JoelSPA <joelarias_0@outlook.es>
2023-03-12 14:52:58 -0400

    first commit

---
+++ database/connection.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+
+const connection = async () => {
+
+    try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/mi_redsocial");
+
+        console.log("MongoDB conectado a db: mi_redsocial");
+
+    } catch (error) {
+        console.log(error);
+        throw new Error("No se  ha podido conectar a la base de datos !!");
+    }
+
+}
+
+module.exports = connection;
---
+++ index.js
@@ -0,0 +1,22 @@
+// Importar dependencias
+const connection = require('./database/connection');
+
+// Mensaje bienvenida
+console.log("API REST - Mi Red Social arrancada")
+
+// Conexion a bbdd
+connection();
+
+// Crear servidor node
+
+
+// Congfigurar cors
+
+
+// Convertir los datos del body a objetos js
+
+
+// Rutas
+
+
+// Poner servidor a escuchar peticiones http

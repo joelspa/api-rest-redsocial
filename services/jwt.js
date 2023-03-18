fc273f799fed466aa8b72d0748d5efb183ccf23e
JoelSPA <joelarias_0@outlook.es>
2023-03-18 16:57:06 -0400

    token

---
+++ services/jwt.js
@@ -0,0 +1,26 @@
+// Importar dependencias
+const jwt = require("jwt-simple");
+const moment = require("moment");
+
+// Clave secreta para codificar y decodificar el token
+const secret = "CLAVE_SECRETA_del_proyecto_987987";
+
+// Crear una funcion para generar tokens
+exports.createToken = (user) => { // Otra manera de exportar la funcion
+    const payload = {
+        sub: user._id,
+        name: user.name,
+        surname: user.surname,
+        nick: user.nick,
+        email: user.email,
+        role: user.role,
+        image: user.image,
+        iat: moment().unix(), // Fecha de creacion del token
+        exp: moment().add(30, "days").unix() // Fecha de expiracion del token
+    };
+
+    // Devolver jwt token codificado
+    return jwt.encode(payload, secret);
+}
+
+// module.exports = createToken;

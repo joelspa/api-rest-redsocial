f9fd4b19237c1632d38255b55ceb721a003fc0e5
JoelSPA <joelarias_0@outlook.es>
2023-03-18 16:57:40 -0400

    login y registro

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,11 @@
 // Importar el modelo de usuario
 const bcrypt = require("bcrypt");
+
+// Importar el modelo de usuario
 const User = require("../models/user");
+
+// Importar el servicio de jwt
+const jwt = require("../services/jwt");
 
 // Acctiones de prueba
 const pruebaUser = (req, res) => {
@@ -56,8 +61,56 @@ const register = (req, res) => {
         })
 }
 
+const login = (req, res) => {
+    // Recoger parametros de la peticion
+    let params = req.body;
+
+    if (!params.email || !params.password) {
+        return res.status(400).json({
+            status: "error",
+            message: "Faltan datos por enviar"
+        })
+    }
+    // Buscar en la db si existe
+    User.findOne({ email: params.email })
+        .then(async user => {
+            if (!user) {
+                return res.status(400).json({
+                    status: "error",
+                    message: "El usuario no existe"
+                })
+            }
+            // Comprobar la contraseña
+            let pwd = await bcrypt.compare(params.password, user.password);
+
+            if (!pwd) {
+                return res.status(400).json({
+                    status: "error",
+                    message: "La contraseña no es correcta"
+                })
+            }
+
+            // Conseguir token
+            const token = jwt.createToken(user);
+
+            // Devolver datos de usuario
+            return res.status(200).json({
+                status: "success",
+                message: "Usuario logueado correctamente",
+                user: {
+                    id: user._id,
+                    name: user.name,
+                    nick: user.nick
+                },
+                token
+            })
+        })
+
+}
+
 // Exportar acciones
 module.exports = {
     pruebaUser,
-    register
+    register,
+    login
 }

a2951d2fa596aad2e235dba238d769df124b9024
JoelSPA <joelarias_0@outlook.es>
2023-04-15 22:28:42 -0400

    controlador de follow

--- controllers/follow.js
+++ controllers/follow.js
@@ -1,3 +1,7 @@
+// Importar el modelo de Follow
+const Follow = require('../models/follow');
+const User = require('../models/user');
+
 
 // Acctiones de prueba
 const pruebaFollow = (req, res) => {
@@ -6,7 +10,33 @@ const pruebaFollow = (req, res) => {
     });
 }
 
+// Acccion para seguir a un usuario
+const save = (req, res) => {
+    // Conseguir datos por body
+
+    // Sacar id del usuario logueado
+
+    // Crear objeto con modelo de Follow
+
+    // Guardar el objeto en la base de datos
+
+
+    return res.status(200).send({
+        status: 'success',
+        message: 'Metodo de follow',
+        identity: req.user
+    });
+}
+
+// Acccion para dejar de seguir a un usuario
+
+// Acccion para obtener los usuarios que sigo
+
+// Acccion para obtener los usuarios que me siguen
+
+
 // Exportar acciones
 module.exports = {
-    pruebaFollow
+    pruebaFollow,
+    save
 }

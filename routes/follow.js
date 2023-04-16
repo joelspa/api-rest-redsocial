af0499dc0eb2c3c57c62f689898aa6a6104d0ec2
JoelSPA <joelarias_0@outlook.es>
2023-04-15 22:29:23 -0400

    ruta de follow

--- routes/follow.js
+++ routes/follow.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const followController = require("../controllers/follow");
+const check = require("../middlewares/auth");
 
 // Definir rutas
 router.get("/prueba-follow", followController.pruebaFollow);
+router.post("/save", check.auth, followController.save); 
 
 // Exportar router
 module.exports = router;

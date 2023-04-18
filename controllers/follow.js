5d1ffdff4aaa1c5d4cee41b721cf8b1cc570f83f
JoelSPA <joelarias_0@outlook.es>
2023-04-17 23:20:59 -0400

    agregada funcion de follow, unfollow y paginacion

--- controllers/follow.js
+++ controllers/follow.js
@@ -2,6 +2,8 @@
 const Follow = require('../models/follow');
 const User = require('../models/user');
 
+// importar depencias
+const mongoosePaginate = require('mongoose-pagination');
 
 // Acctiones de prueba
 const pruebaFollow = (req, res) => {
@@ -13,30 +15,161 @@ const pruebaFollow = (req, res) => {
 // Acccion para seguir a un usuario
 const save = (req, res) => {
     // Conseguir datos por body
+    const params = req.body;
 
     // Sacar id del usuario logueado
+    const identity = req.user;
 
     // Crear objeto con modelo de Follow
-
-    // Guardar el objeto en la base de datos
-
-
-    return res.status(200).send({
-        status: 'success',
-        message: 'Metodo de follow',
-        identity: req.user
+    let userToFollow = new Follow({
+        user: identity.id,
+        followed: params.followed
     });
+
+    // Guardar el objeto en la base de datos con mongoose 7.0.0
+    userToFollow.save().then(followStored => {
+        if (!followStored) {
+            return res.status(404).send({
+                status: 'error',
+                message: 'El seguimiento no se ha guardado'
+            });
+        }
+
+        return res.status(200).send({
+            status: 'success',
+            message: 'Metodo de follow',
+            identity: req.user,
+            follow: followStored
+        });
+    }).catch(err => {
+        return res.status(500).send({
+            status: 'error',
+            message: 'Error al guardar el seguimiento'
+        });
+    });
+
+    // Guardar el objeto en la base de datos con venrsiones anteriores a mongoose 7.0.0
+    //    userToFollow.save((err, followStored) => {
+    //        if (err) {
+    //            return res.status(500).send({
+    //                status: 'error',
+    //                message: 'Error al guardar el seguimiento'
+    //            });
+    //        }
+    //
+    //
+    //        return res.status(200).send({
+    //            status: 'success',
+    //            message: 'Metodo de follow',
+    //            identity: req.user,
+    //            follow: followStored
+    //        });
+    //    });
+
 }
 
 // Acccion para dejar de seguir a un usuario
+const unfollow = (req, res) => {
+    // Recoger el id del usuario logueado
+    const identity = req.user.id;
 
-// Acccion para obtener los usuarios que sigo
+    // Recoger el id del usuario que se quiere dejar de seguir
+    const followedId = req.params.id;
 
-// Acccion para obtener los usuarios que me siguen
+    // Find de las coincidencias y hacer remove
+    Follow.findOneAndDelete({
+        user: identity,
+        followed: followedId
+    }).then(followRemoved => {
+        if (!followRemoved) {
+            return res.status(404).send({
+                status: 'error',
+                message: 'no se ha encontrado el follow'
+            });
+        }
+        return res.status(200).send({
+            status: 'success',
+            message: 'El seguimiento se ha eliminado correctamente',
+            follow: followRemoved
+        });
+    }).catch(err => {
+        return res.status(500).send({
+            status: 'error',
+            message: 'Error al dejar de seguir',
+            error: err
+        });
+    });
+}
+// Acccion listado de usarios que cualquier usuario esta siguiendo (siguiendo)
+const following = (req, res) => {
+    // Recoger el id del usuario logueado
+    let userId = req.user.id;
 
+    // Comprobar si se esta enviando el id por la url
+    if (req.params.id) userId = req.params.id;
+
+    // Comprobar si me llega la pagina, si no me llega la pagina por defecto es 1
+    let page = 1;
+
+    if (req.params.page) page = req.params.page;
+
+    // Usuarios por pagina quiero mostrar
+    const itemsPerPage = 5;
+
+    // fin a follow, popular datos de los usuarios y paginar con mongoose paginate, mongoose 7.0.0
+    Follow.countDocuments({ user: userId })
+        .then(count => {
+            Follow.find({ user: userId })
+                .populate("user followed", "-password -__v -role") // usar - delante de los campos que no quiero que me devuelva
+                .paginate(page, itemsPerPage)
+                .then(follows => {
+                    {
+                        if (!follows) {
+                            return res.status(404).send({
+                                status: 'error',
+                                message: 'No se ha encontrado ningun follow'
+                            });
+                        }
+                        return res.status(200).send({
+                            status: 'success',
+                            follows,
+                            page,
+                            total: count,
+                            pages: Math.ceil(count / itemsPerPage)
+                        });
+                    }
+                }
+                ).catch(err => {
+                    return res.status(500).send({
+                        status: 'error',
+                        message: 'Error al devolver el follow',
+                        error: err
+                    });
+                });
+        }
+        ).catch(err => {
+            return res.status(500).send({
+                status: 'error',
+                message: 'Error al devolver el follow',
+                error: err
+            });
+        });
+}
+
+// Acccion listado de usuarios que siguen a cualquier otro usuario (seguidores)
+const followers = (req, res) => {
+    return res.status(200).send({
+        status: 'success',
+        message: 'Metodo de followers'
+    });
+}
 
 // Exportar acciones
 module.exports = {
     pruebaFollow,
-    save
+    save,
+    unfollow,
+    following,
+    followers
 }
+
--- routes/follow.js
+++ routes/follow.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const followController = require("../controllers/follow");
+const FollowController = require("../controllers/follow");
 const check = require("../middlewares/auth");
 
 // Definir rutas
-router.get("/prueba-follow", followController.pruebaFollow);
-router.post("/save", check.auth, followController.save); 
+router.get("/prueba-follow", FollowController.pruebaFollow);
+router.post("/save", check.auth, FollowController.save); 
+router.delete("/unfollow/:id", check.auth, FollowController.unfollow);
+router.get("/following/:id?/:page?", check.auth, FollowController.following);
+router.get("/followers/:id?/:page?", check.auth, FollowController.followers);
+
 
 // Exportar router
 module.exports = router;

24a34f8a14ee7b84431b11c85b9be0a89881da29
JoelSPA <joelarias_0@outlook.es>
2023-05-03 20:29:05 -0400

    Importar modelo publication

--- controllers/publication.js
+++ controllers/publication.js
@@ -1,10 +1,26 @@
 
+const Publication = require("../models/publication");
+
 // Acctiones de prueba
 const pruebaPublication = (req, res) => {
     res.status(200).send({
         message: 'Hola mundo desde: constrollers/publication.js'
     });
 }
+
+// Guardar publicación
+
+// Sacar una publicación
+
+// Eliminar una publicación
+
+// Listar publicaciones
+
+// Listar publicaciones de un usuario
+
+// Subir ficheros
+
+// Devolver archivo multimedia ficheros
 
 // Exportar acciones
 module.exports = {

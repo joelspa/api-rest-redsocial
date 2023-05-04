45101780955a292868424f30fefcc6d59f1b7d62
JoelSPA <joelarias_0@outlook.es>
2023-05-03 21:08:06 -0400

    rutas actualizadas

--- routes/publication.js
+++ routes/publication.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const publicationController = require("../controllers/publication");
+const chek = require("../middlewares/auth");
 
 // Definir rutas
 router.get("/prueba-publication", publicationController.pruebaPublication);
+router.post("/save", chek.auth, publicationController.save);
+router.get("/detail/:id", chek.auth, publicationController.detail);
+router.delete("/delete/:id", chek.auth, publicationController.remove);
 
 // Exportar router
 module.exports = router;
